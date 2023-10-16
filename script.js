7a04f52bf14bf781fa480eacdb0fdb15abb5696d
sreejithkr047 <sreejithkr047@gmail.com>
2023-10-17 02:57:06 +0530

    Implemented Etch-A-Sketch

--- script.js
+++ script.js
@@ -1,21 +1,61 @@
+function draw()
+{
 const container=document.querySelector('.container');
 let square=[];
-for(let i=0;i<16;i++)
-{     
-     square[i] = [];     
-     for(let j=0;j<16;j++)
-     {  square[i][j]=document.createElement('div');
-        container.appendChild(square[i][j]);
-        square[i][j].classList.add('square'); 
+let clicked=0;
+let side=16;
 
-     }
+function creategrid()
+{
+    for(let i=0;i<side;i++)
+    {     
+        square[i] = [];     
+        for(let j=0;j<side;j++)
+        {  square[i][j]=document.createElement('div');
+            container.appendChild(square[i][j]);
+            square[i][j].classList.add('square'); 
+            square[i][j].addEventListener('click', () => {
+                if(clicked==1)
+                    clicked=0;
+                else
+                    clicked=1;
 
+                square[i][j].classList.toggle('color');
+            
+            });  
+
+            square[i][j].addEventListener('mouseover',()=>
+            {   if(clicked==1)
+                { square[i][j].classList.add('color');}
+            });  
+
+            
+        }
+
+    }   
 }   
 
-const click=document.querySelectorAll('.square');
-click.forEach(box=>{
-    box.addEventListener('click',()=>{
-
-    });
-
+creategrid(); 
+const reset=document.querySelector('#reset');
+reset.addEventListener('click',()=>
+{   for(let i=0;i<side;i++)
+    {    
+        for(let j=0;j<side;j++)
+        {   
+            square[i][j].classList.remove('color');
+        }
+    }
+    clicked=0;     
 });
+
+const resize=document.querySelector('#resize');
+resize.addEventListener('click',()=>
+{  side=prompt(" number of squares per side for the new grid?");     
+   container.innerHTML='';
+   creategrid(); 
+});
+
+}
+
+draw();
+
